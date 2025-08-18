badc91d5ef5078c9466b5064eab5f8f03935ae98
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-08-17 20:30:20 -0500

    🎉 feat: agregar archivo con ejemplos básicos de variables
    - Crear `01-const-let.ts` con ejemplos de const, let y tipos básicos
    - Importar y registrar el archivo en `main.ts` para su ejecución

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+const firstName: string = 'Alonso';
+const lastName: string = 'Anchante';
+
+let diceNumber: number = 1;
+
+const containsLetterA: boolean = lastName.includes('a');
+
+console.log({containsLetterA, diceNumber});
+
+console.log({firstName, lastName});
